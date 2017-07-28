abcf62d3411062a271e3d5acd0b5ba9e4abf6de3
pvighi <patricio.vighi@guardian.co.uk>
2017-07-28 11:17:13 +0100

    added api gateway and some debug output to figure out how to handle xml requests

--- src/main/scala/com/gu/salesfoce/messageHandler/Lambda.scala
+++ src/main/scala/com/gu/salesfoce/messageHandler/Lambda.scala
@@ -8,11 +8,11 @@ import org.slf4j.{ Logger, LoggerFactory }
  * You can test your lambda by sending it the following payload:
  * {"name": "Bob"}
  */
-class LambdaInput() {
-  var name: String = _
-  def getName(): String = name
-  def setName(theName: String): Unit = name = theName
-}
+//class LambdaInput() {
+//  var name: String = _
+//  def getName(): String = name
+//  def setName(theName: String): Unit = name = theName
+//}
 
 case class Env(app: String, stack: String, stage: String) {
   override def toString: String = s"App: $app, Stack: $stack, Stage: $stage\n"
@@ -32,10 +32,11 @@ object Lambda {
   /*
    * This is your lambda entry point
    */
-  def handler(lambdaInput: LambdaInput, context: Context): Unit = {
+  def handler(lambdaInput: String, context: Context): Unit = {
     val env = Env()
     logger.info(s"Starting $env")
-    logger.info(process(lambdaInput.name, env))
+    //logger.info(lambdaInput)
+    logger.info(process("world", env))
   }
 
   /*
