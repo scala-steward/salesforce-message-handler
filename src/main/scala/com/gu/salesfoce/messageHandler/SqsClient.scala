89a97f83474bbe1ece5f5978a992a432918b9aae
pvighi <patricio.vighi@guardian.co.uk>
2017-08-21 11:27:14 +0100

    remove message group id as the queue is not fifo anymore

--- src/main/scala/com/gu/salesfoce/messageHandler/SqsClient.scala
+++ src/main/scala/com/gu/salesfoce/messageHandler/SqsClient.scala
@@ -32,11 +32,8 @@ object SqsClient extends QueueClient with Logging {
     def sendToQueue(msg: String): SendMessageResult = {
       logger.info(s"sending to queue $queueUrl")
       val sendMessageRequest = new SendMessageRequest(queueUrl, msg)
-      //TODO if we parsed the xml message we could use account id or something like that as messageGroupId
-      sendMessageRequest.setMessageGroupId("messageGroup1")
       sqsClient.sendMessage(sendMessageRequest)
     }
-
     Future {
       Try(sendToQueue(message))
     }
